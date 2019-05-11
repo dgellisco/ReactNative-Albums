b86979820a6e866c076255e1e0861e76e438cda0
David Ellis <dgellis@live.com>
2019-05-11 10:31:40 -0600

    Add axios http request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>AlbumList</Text>
-        </View>
-    );
-};
+class AlbumList extends Component {
+    state = {
+        albums: []
+    };
+    
+    componentWillMount() {
+        // Axios http request (inherently asynchronous)
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            // Returned promise.  Request handler calls 'setState'.
+            .then(response => this.setState({ albums: response.data }));
+    }
+    
+    render() {
+        console.log(this.state);
+
+        return (
+            <View>
+                <Text>AlbumList!!!</Text>
+            </View>
+        );
+    }
+}
 
 const styles = {
 
